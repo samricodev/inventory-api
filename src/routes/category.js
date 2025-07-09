ecfab18a3b0a720a5d26291a500587f2bb17aa0e
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-07-09 12:45:27 -0600

    feat: refactor route handlers to use controller methods directly and improve test data extraction

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,17 +1,17 @@
-const {
-    getCategories,
-    getCategory,
-    createCategory,
-    updateCategory,
-    deleteCategory
-} = require('../controllers/category');
+const categoryController = require('../controllers/category');
 const registerCategoryValidation = require('../middlewares/registerCategoryValidation');
 const CategoryRouter = require('express').Router();
 
-CategoryRouter.get('/', getCategories);
-CategoryRouter.get('/:id', getCategory);
-CategoryRouter.post('/',[ registerCategoryValidation, createCategory ]);
-CategoryRouter.put('/:id',[ registerCategoryValidation, updateCategory ]);
-CategoryRouter.delete('/:id', deleteCategory);
+CategoryRouter.get('/', categoryController.getCategories);
+CategoryRouter.get('/:id', categoryController.getCategory);
+CategoryRouter.post('/',[ 
+    registerCategoryValidation, 
+    categoryController.createCategory 
+]);
+CategoryRouter.put('/:id',[ 
+    registerCategoryValidation, 
+    categoryController.updateCategory 
+]);
+CategoryRouter.delete('/:id', categoryController.deleteCategory);
 
 module.exports = CategoryRouter;
--- src/routes/item.js
+++ src/routes/item.js
@@ -4,8 +4,14 @@ const ItemRouter = require('express').Router();
 
 ItemRouter.get('/', itemController.getItems);
 ItemRouter.get('/:id', itemController.getItem);
-ItemRouter.post('/', [ registerItemValidation, itemController.createItem ]);
-ItemRouter.put('/:id',[ registerItemValidation, itemController.updateItem ]);
+ItemRouter.post('/', [ 
+    registerItemValidation, 
+    itemController.createItem 
+]);
+ItemRouter.put('/:id',[ 
+    registerItemValidation, 
+    itemController.updateItem 
+]);
 ItemRouter.delete('/:id', itemController.deleteItem);
 
 module.exports = ItemRouter;
