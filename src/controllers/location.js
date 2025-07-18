0eb02e823f90057c4dde494250d83afd95f52706
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-07-18 11:38:06 -0600

    fix: give req.body to update in location update hahahahaha x2

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -45,7 +45,7 @@ const updateLocation = async (req, res) => {
     const location = await Location.findByIdAndUpdate({
       _id: req.params.id,
       userId: req.user.id
-    });
+    }, req.body, { new: true });
     if (!location) return res.status(404).json(response.error(404, res.translate('Location not found')));
     res.status(200).json(response.success(200, res.translate('Location updated'), location));
   } catch (error) {
